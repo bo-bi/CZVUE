b0efd5a41c07a172ab7cf6d44bcf3d7873857fc0
WH <18935445147@qq.com>
2018-05-04 17:27:56 +0800

    增加: script中export default配置

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        我们的第一个Vue程序
+        我们的第一个Vue程序<br/>
+        {{text}}
+        <br/>
+        <!--双向数据绑定-->
+        输入：<input type="text" v-model="text"/>
+        <!--列表-->
+        <ul>
+            <li v-for="person in list">{{person.name}}</li>
+        </ul>
     </div>
     <!--<div>-->
         <!--只能包含一个根结点2.x以后-->
@@ -8,9 +16,27 @@
 </template>
 
 <script>
-    // export default {
-    //     name: "app"
-    // }
+    export default {
+        //配置
+        //类似 $scope.xxx = '初始化'
+        // data: function() {
+        //
+        // }
+        data() {
+            return {
+                //放数据的地方
+                text: '大家好',
+                list: [
+                    {
+                        name: 'jack'
+                    },
+                    {
+                        name: 'rouse'
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style scoped>

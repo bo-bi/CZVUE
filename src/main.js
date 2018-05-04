9a3044ed8c9e4a6733f2038540814dec3ca279ac
WH <18935445147@qq.com>
2018-05-04 11:12:51 +0800

    增加：全体引入

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+// //默认引入
 import Cal from './cal.js';
+// //声明式引入
 import {obj1,obj2} from './cal.js';
 import {obj4} from './cal.js';
 
@@ -6,3 +8,9 @@ console.log(Cal);
 console.log(obj1);
 console.log(obj2);
 console.log(obj4);
+
+//--------------------------------------------------------------------------------
+//全体引入
+import * as allObj from './cal.js';
+
+console.log(allObj);

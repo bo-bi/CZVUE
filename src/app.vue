24674bbbf5af111954ef346d950b83126fa91229
WH <18935445147@qq.com>
2018-05-07 15:37:19 +0800

    增加:  :class样式绑定

--- src/app.vue
+++ src/app.vue
@@ -9,19 +9,19 @@
             * v-model 双向数据绑定，v-bind是单向数据绑定（js内存改变影响页面）
         </pre>
 
-        v-text：
+        <span :class="{'red':'isTitle'}">v-text：</span>
         <span v-text="text"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" v-text="text" />
         <hr />
-        v-html:
+        <span :class="{'red':'isTitle'}">v-html:</span>
         <span v-html="html"></span>
         <hr />
-        v-if:
+        <span :class="{'red':'isTitle'}">v-if:</span>
         <div v-if="isShow" style="width: 600px;height: 100px;background-color: red;"></div>
         <hr />
-        v-show:
+        <span :class="{'red':'isTitle'}">v-show:</span>
         <div v-show="isShow" style="width: 600px;height: 100px;background-color: green;"></div>
-        <<hr />
-        v-model:
+        <hr />
+        <span :class="{'red':'isTitle'}">v-model:</span>
         <br />
         页面变化<input type="text" v-model="mTest" />
         <br />
@@ -29,10 +29,39 @@
         <br />
         <!--v-bind:单项数据绑定，给下面的input的value值赋值用v-bind:value="mTest"，js改变内容改变，内容改变，js不改变-->
         <input id="val" type="text" v-bind:value="mTest">
+
+        <br />
+        <br />
+        <br />
+        <hr />
+        <span :class="{'red':'isTitle'}">v-bind:class（:class）</span>
+        <div v-bind:class="isRed?'red':'pink'">单个class</div>
+        <div :class="{'red':isRed,'big':true}">多个class</div>
+        <br />
+        复杂情况，通过遍历，根据当前对象的成绩，匹配成绩和样式的清单对象，用成绩做key，取对象的value，最终返回字符串做样式名
+        <ul>
+            <li v-for="stu in stus">
+                <span :class="{'A':'red','B':'green'}[stu.score]">name:{{stu.name}}    score:{{stu.score}}</span>
+            </li>
+        </ul>
+
+        <br />
+        <br />
+        <br />
+        <ul>
+            <li v-for="one in fruit" :class="{'bannana':'yellow','apple':'red','pineapple':'green'}[one.name]">
+                  {{one.name}}
+            </li>
+        </ul>
+        <hr />
     </div>
 </template>
 
 <script>
+    //class 取一个 返回一个字符串
+    //      取多个 返回一个对象
+    //一个学生列表 每个人都有成绩A或B,根据当前学生的成绩匹配显示不同的颜色
+    //A-red B-green
     export default {
         data() {
             return {
@@ -45,13 +74,46 @@
                         </ul>
                 `,
                 isShow: false,
-                mTest: '你好a我们是什么啊'
-
+                mTest: '你好a我们是什么啊',
+                isRed: false,
+                isTitle: true,
+                stus: [
+                    {
+                        name: 'jack',
+                        score: 'A'
+                    },
+                    {
+                        name: 'rose',
+                        score: 'B'
+                    }
+                ],
+                fruit: [
+                    {
+                        name: 'bannana'
+                    },
+                    {
+                        name: 'apple'
+                    },
+                    {
+                        name: 'pineapple'//菠萝
+                    }
+                ]
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .red {
+        background-color: red;
+    }
+    .green {
+        background-color: green;
+    }
+    .yellow {
+        background-color: yellow;
+    }
+    .big {
+        font-size: 30px;
+    }
 </style>

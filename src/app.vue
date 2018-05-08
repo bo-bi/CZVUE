fb94968612e287f893ed3ffff0d7f40aaf8b7251
WH <18935445147@qq.com>
2018-05-08 14:09:39 +0800

    增加：添加和删除功能

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,24 @@
 <template>
     <!--你好 这是错误的，必须有且只能有一个根节点！-->
     <div>
-
+        <ul>
+            <li v-for="(hero,index) in heros" :key="index" :class="{'A':'red','B':'green','C':'blue','D':'pink'}[hero.score]">
+                {{hero.name}},{{hero.score}}
+                <button @click="del(index)">删除</button>
+            </li>
+        </ul>
+        英雄姓名：<input type="text" v-model="name" />
+        英雄成绩：<input type="text" v-model="score" />
+        <button v-on:click="addHero">添加英雄</button>
     </div>
 </template>
-
+B
 <script>
     export default {
         data() {
             return {
+               name: '',
+               score: '',
                heros: [
                    {
                        id: 1,
@@ -18,7 +28,7 @@
                    {
                        id: 2,
                        name: '日女',
-                       score: ''
+                       score: 'B'
                    },
                    {
                        id: 3,
@@ -30,12 +40,38 @@
                        name: '扎克',
                        score: 'D'
                    },
-               ]
+               ],
+            }
+        },
+        methods: {
+            addHero() {
+                //获取页面输入的值的时候，你用什么？v-model
+                this.heros.push({
+                        name: this.name,
+                        score: this.score
+                });
+                //添加完 清空input
+                this.name = '';
+                this.score = '';
+            },
+            del(index) {
+                this.heros.splice(index,1);
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .red {
+        background-color: red;
+    }
+    .green {
+        background-color: yellowgreen;
+    }
+    .blue {
+        background-color: skyblue;
+    }
+    .pink {
+        background-color: hotpink;
+    }
 </style>

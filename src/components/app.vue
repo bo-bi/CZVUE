ab223e5a2be921021f1d49d7eb80daf5c4d6d3eb
WH <18935445147@qq.com>
2018-05-16 15:07:02 +0800

    增加: vue-resource get、post

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <div class="header">头部</div>
-        <router-view class="main"></router-view>
-        <div class="footer">底部</div>
+        {{data}}
     </div>
 </template>
 
@@ -10,23 +8,36 @@
     export default {
         data() {
             return {
-
+                 data: []
             }
+        },
+        //数据初始化，但是未生成DOM
+        created() {
+            // console.log(this.$http);
+            // https://api.douban.com/v2/movie/celebrity/1054395
+            // this.$http.get('http://127.0.0.1:8085/json/nav.json').then(res=>{
+            //     console.log(res);
+            //     this.data = res.body.resInfo;
+            // },err=>{
+            //     console.log(err)
+            // });
+
+            //发表评论 跨域不能使用
+            // http://bang.duia.com/web/reply/publish?tid=756472&fr=0&s=0&ct=321
+
+            this.$http.post('http://127.0.0.1:8085/json/nav.json',{
+                content: '哈哈'
+            },{
+                emulateJSON: true
+            }).then(res=>{
+                console.log(res,"res");
+            },err=>{
+                console.log(err);
+            });
         }
     }
 </script>
 
 <style scoped>
-    .header {
-        height: 100px;
-        background-color: pink;
-    }
-    .main {
-        height: 100px;
-        background-color: skyblue;
-    }
-    .footer {
-        height: 100px;
-        background-color: yellowgreen;
-    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,31 +1,19 @@
 //引入Vue
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-//安装插件
-Vue.use(VueRouter);
+//引入vue-resource
+import VueResource from 'vue-resource';
 
 //引入主体
 import App from './components/app.vue';
-import Home from './components/home.vue';
-import NotFoundVue from './components/notFound.vue'
 
-//创建路由对象并配置路由规则
-let router = new VueRouter();
-router.addRoutes([
-    //重定向
-    //①通过path 两处都要改
-    // {path: '/', redirect: '/home'},
-    // {path: '/home', component: Home}
+//安装插件
+Vue.use(VueResource);//插件都是挂载属性
+//未来通过this.$http
+//Vue 是所有实例对象的构造函数
+//通过在原型上添加属性来进行挂载的 Vue.prototype.$http -> 实例(this)就可以.$http
 
-    //②通过name,不改path
-    {path: '/', redirect: {name: 'home'}},
-    {name: 'home', path: '/home', component: Home},
-    //最终无法匹配 -> 404
-    {path: '*', component: NotFoundVue}
-]);
 //new Vue 启动Vue
 new Vue({
     el: '#app',
-    router,
     render: c => c(App)
 });

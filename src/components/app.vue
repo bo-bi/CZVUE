1b086e04508afeff4910ab631d5c34a4dadf1957
WH <18935445147@qq.com>
2018-05-16 17:25:18 +0800

    增加: axios get、post

--- src/components/app.vue
+++ src/components/app.vue
@@ -13,27 +13,45 @@
         },
         //数据初始化，但是未生成DOM
         created() {
-            // console.log(this.$http);
-            // https://api.douban.com/v2/movie/celebrity/1054395
-            // this.$http.get('http://127.0.0.1:8085/json/nav.json').then(res=>{
-            //     console.log(res);
-            //     this.data = res.body.resInfo;
-            // },err=>{
-            //     console.log(err)
-            // });
+           this.$axios.get('getlunbo',{
+               params: {
+                   id: 123
+               },
+               // baseURL: 'http://www.baidu.com',
+               // headers: {
+               //
+               // }
+           })
+               .then(res=>{
+                   console.log(res);
+                   this.data = res.data.resInfo;
+               })
+               .catch(err=>{
+                   console.log(err);
+               })
+               .finally(()=>{
+                   console.log("最终");
+               });
 
-            //发表评论 跨域不能使用
-            // http://bang.duia.com/web/reply/publish?tid=756472&fr=0&s=0&ct=321
-
-            this.$http.post('http://127.0.0.1:8085/json/nav.json',{
-                content: '哈哈'
-            },{
-                emulateJSON: true
-            }).then(res=>{
-                console.log(res,"res");
-            },err=>{
-                console.log(err);
-            });
+           //post
+           this.$axios.post('postcomment/300',
+               'content=这是一段内容'
+               // {
+               //     content: '这是一段内容'
+               // },
+               // {
+               //     //设置
+               //     headers: {
+               //          'content-type': 'application/x-www-form-urlencoded'
+               //     }
+               // }
+           )
+           .then(res=>{
+               this.data = res.data.message;
+           })
+           .catch(err=>{
+               console.log(err);
+           });
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 //引入Vue
 import Vue from 'vue';
-//引入vue-resource
-import VueResource from 'vue-resource';
 
 //引入主体
 import App from './components/app.vue';
 
-//安装插件
-Vue.use(VueResource);//插件都是挂载属性
-//未来通过this.$http
-//Vue 是所有实例对象的构造函数
-//通过在原型上添加属性来进行挂载的 Vue.prototype.$http -> 实例(this)就可以.$http
+//引入
+import Axios from 'axios';
+//设置全局公共路径
+Axios.defaults.baseURL = 'http://182.254.146.100:8899/api/';
+
+//给Vue原型挂载一个属性
+Vue.prototype.$axios = Axios;
+
 
 //new Vue 启动Vue
 new Vue({

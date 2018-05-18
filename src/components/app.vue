cc241bc4a7410905185c2193ace5ffb09d85d1c2
WH <18935445147@qq.com>
2018-05-18 10:32:08 +0800

    增加: watch 基本、复杂数据类型

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-         <button @click="send">发送请求1</button>
-         <button @click="send2">发送请求2</button>
+        <!--<input type="text" v-model="text"/>-->
+        <!--{{text}}-->
+
+        <button @click="change">改变值</button>
     </div>
 </template>
 
@@ -10,27 +12,34 @@
     export default {
         data() {
             return {
-                 data: []
+                text: '456',
+                person: [
+                    {name: 'jack'},
+                    {name: 'rose'}
+                ]
             }
         },
         methods: {
-            send() {
-                this.$axios.get('http://127.0.0.1:8085/json/nav.json')
-                .then(res=>{
-                    console.log(res);
-                })
-                .catch(err=>{
-                    console.log(err);
-                })
+            change() {
+                // this.text = '123';
+                this.person[1].name = 'mick';
+            }
+        },
+        watch: {
+            //基本数据类型
+            text: function(newV,oldV) {
+                console.log('text值发生了改变');
+                console.log(newV);
+                console.log(oldV);
             },
-            send2() {
-                this.$axios.get('http://127.0.0.1:8085/json/nav.json')
-                .then(res=>{
-                    console.log(res);
-                })
-                .catch(err=>{
-                    console.log(err);
-                })
+            //复杂数据类型(数组、对象)
+            person: {
+                handler: function (newV, oldV) {
+                    console.log("数组的值发生了变化");
+                    console.log(newV);
+                    console.log(oldV);
+                },
+                deep: true
             }
         }
     }

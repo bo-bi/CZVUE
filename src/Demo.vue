3626c1696a48a1b6d727695bb6dc98152fb4a1e7
WH <18935445147@qq.com>
2019-09-11 17:04:04 +0800

    面试遇到的nextTick问题

---
+++ src/Demo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <button @click="useFatherMethod">子组件调用父组件的方法</button>
+    <button @click="change">更换图片</button>
+    <button @click="changeDiv">更换div</button>
+    <br />
+    <img ref="img" :src="src" alt="" />
+    <div ref="div" :style="style"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      src: "../img/336*180.png",
+      style: "width: 100px; height: 100px; background: pink;"
+    }
+  },
+  methods: {
+    useFatherMethod() {
+      // this.$parent.fatherMethod();
+      // this.$emit("useFather");
+      // this.$parent.msg
+    },
+    change() {
+      this.src = "../img/703*376.png";
+      console.log(this.$refs.img.src, 'offsetHeight');
+      this.$nextTick(function() {
+        console.log(this.$refs.img.src);
+      });
+      this.src = "../img/2360*700.jpg";
+
+      // this.$nextTick(() => {
+      //   console.log(this.$refs.img.src, 'offsetHeight');
+      // })
+      
+    },
+    changeDiv() {
+      this.style = "width: 100px; height: 200px; background: pink;";
+      console.log(this.$refs.div.offsetHeight);
+      this.$nextTick(function() {
+        console.log(this.$refs.div.offsetHeight);
+      })
+      this.style = "width: 100px; height: 300px; background: pink;";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
--- src/app.vue
+++ src/app.vue
@@ -2,10 +2,14 @@
     <div>
         <p ref="p">{{ msg }}</p>
         <button @click="changeValue">更改值</button>
+        <button @click="fetchSon">获取子组件信息</button>
+        <hr />
+        <demo-vue @useFather="fatherMethod" ref="demo"></demo-vue>
     </div>
 </template>
 
 <script>
+import DemoVue from './Demo.vue';
 // 怎么在DOM更新后,在做事情?
 // 为了在数据变化之后等待 Vue 完成更新 DOM，可以在数据变化之后立即使用 Vue.nextTick(callback)。这样回调函数将在 DOM 更新完成后被调用。
     export default {
@@ -14,7 +18,13 @@
                 msg: '未更新',
             }
         },
+        components: {
+            DemoVue,
+        },
         methods: {
+            fatherMethod() {
+                console.log("我是父组件的方法");
+            },
             changeValue() {
                 this.msg = '已更新';
                 // 这里查看实际的文本(this.$refs.p.innerHTML),并不是查看data(this.person.name)
@@ -22,6 +32,9 @@
                 this.$nextTick( () => {
                     console.log(this.$refs.p.innerHTML); // 已更新
                 })
+            },
+            fetchSon() {
+                console.log(this.$refs.demo)
             }
         }
     }
